216126d6592172ac09b44d24b4eef8c4326ed10b
Chris Agbolade <yemluck@gmail.com>
2025-02-13 23:42:33 -0600

    implemented two pointers approach to solve the problem

--- two_pointers/make_square.py
+++ two_pointers/make_square.py
@@ -7,3 +7,40 @@ Input: [-2, -1, 0, 2, 3]
 Output: [0, 1, 4, 4, 9]
 """
 
+# Solution
+#1 Naive Solution/Brute force
+
+def make_square1(arr):
+    output = []
+    for num in arr:
+        output.append(num**2)
+    output.sort()
+    return output
+
+# print(make_square1([2,4,-1]))
+
+
+#2: Using two pointers
+def make_square2(arr):
+    output = [0 for _ in range(len(arr))]
+    length = len(arr) - 1
+    left, right = 0, length
+    while left <= right:
+        left_square = arr[left]**2
+        right_square = arr[right]**2
+
+        if left_square > right_square:
+            output[length] = left_square
+            left += 1
+            print("if 1", output, "this is left", left)
+        else:
+            output[length] = right_square
+            right -= 1
+            print("elif2", output, "this is right", right)
+
+        length -= 1
+        print(f"this is the length: {length}")
+    return output
+
+
+print(make_square2([-5,2,3, 4,5]))
