daf451ee201ab914b83a21bb482847608574fd66
Chris Agbolade <yemluck@gmail.com>
2025-02-18 15:53:01 -0600

    implemented naive solution approach

--- two_pointers/strings_with_backspaces.py
+++ two_pointers/strings_with_backspaces.py
@@ -21,4 +21,30 @@ Explanation: After applying backspaces, the strings become "x" and "x" respectiv
 # Naive approach
 
 def compare_strings(str1, str2):
-    pass
+    hash_count1 = 0
+    hash_count2 = 0
+    word1 = list(str1)
+    word2 = list(str2)
+
+    for i in word1:
+        if i == "#":
+            hash_count1 += 1
+
+    for j in word2:
+        if j == "#":
+            hash_count2 += 1
+
+    while hash_count1 > 0:
+        word1.pop(word1.index('#') - 1) # this removes the first char before the first '#'
+        word1.pop(word1.index('#')) # this gets the first occurrence of '#' in the word
+        hash_count1 -= 1
+
+    while hash_count2 > 0:
+        word2.pop(word2.index('#') - 1)
+        word2.pop(word2.index('#'))
+        hash_count2 -= 1
+
+    return word1 == word2
+
+
+print(compare_strings("xy#z", "xzzz##"))
