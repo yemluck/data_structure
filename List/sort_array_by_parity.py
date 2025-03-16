f1468fdd473b1e4afb624327c94a299c54aa430d
Chris Agbolade <yemluck@gmail.com>
2025-03-16 15:45:18 -0500

    optimized the solution

--- List/sort_array_by_parity.py
+++ List/sort_array_by_parity.py
@@ -17,4 +17,13 @@ Output: [0]
 
 
 def sort_array_by_parity(nums):
-    pass
+    l = 0
+    for r in range(len(nums)):
+        if nums[r] % 2 == 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+
+    return nums
+
+print(sort_array_by_parity([3,1,2,4]))
+print(sort_array_by_parity([0]))
