0cc44abac48762acb1c43e37b62d9479fb78b368
Chris Agbolade <yemluck@gmail.com>
2025-03-17 00:50:09 -0500

    maximum consecutive ones , slide window problem

---
+++ List/max_consecutive_ones.py
@@ -0,0 +1,21 @@
+"""
+Given a binary array nums, return the maximum number of consecutive 1's in the array if you can flip at most one 0.
+
+Example 1:
+Input: nums = [1,0,1,1,0]
+Output: 4
+Explanation:
+- If we flip the first zero, nums becomes [1,1,1,1,0] and we have 4 consecutive ones.
+- If we flip the second zero, nums becomes [1,0,1,1,1] and we have 3 consecutive ones.
+The max number of consecutive ones is 4.
+
+Example 2:
+Input: nums = [1,0,1,1,0,1]
+Output: 4
+Explanation:
+- If we flip the first zero, nums becomes [1,1,1,1,0,1] and we have 4 consecutive ones.
+- If we flip the second zero, nums becomes [1,0,1,1,1,1] and we have 4 consecutive ones.
+The max number of consecutive ones is 4.
+"""
+
+# SLIDING WINDOW
