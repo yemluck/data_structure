79b49dc6a0e74939711883c2dcaf635722b454ef
Chris Agbolade <yemluck@gmail.com>
2025-03-19 16:04:15 -0500

    two pointer approach

--- List/sorted_squares.py
+++ List/sorted_squares.py
@@ -25,4 +25,18 @@ solution using a different approach?
 """
 
 def sorted_squares(nums):
-    pass
+    n = len(nums)
+    result = [0] * n
+    left, right = 0, n-1
+
+    for i in range(n-1, -1, -1):
+        if abs(nums[left] < abs(nums[right])):
+            square = nums[right]
+            right -= 1
+        else:
+            square = nums[left]
+            left += 1
+        result[i] = square * square
+
+    return result
+
