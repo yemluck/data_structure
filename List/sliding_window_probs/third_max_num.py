0e2fa73689351635e7d370e1aa509d2549384da4
Chris Agbolade <yemluck@gmail.com>
2025-03-19 15:12:25 -0500

    third max number, sort didn't work

--- List/sliding_window_probs/third_max_num.py
+++ List/sliding_window_probs/third_max_num.py
@@ -27,4 +27,27 @@ The third distinct maximum is 1.
 """
 
 def third_max(nums):
-    pass
+    big = bigger = float("-inf")
+    biggest = nums[0]
+
+    for num in nums:
+        if num > biggest:
+            biggest, bigger, big = num, biggest, bigger
+        elif biggest >num > bigger:
+            bigger, big = num, bigger
+        elif bigger > num > big:
+            big = num
+
+    if big > float("-inf"):
+        return big
+    else:
+        return biggest
+
+    # print(f"this is big  {big}, this is bigger  {bigger} and this is biggest  {biggest}")
+    # print(f"this is big  {big}, this is bigger  {bigger} and this is biggest  {biggest}")
+
+
+print(third_max([2,2,3,1]))
+print(third_max([1,2]))
+print(third_max([3,2,1]))
+print(third_max([-1,2,3]))
