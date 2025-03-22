53565946b30044a8f52e00d67f6d7e6931c3e0aa
Chris Agbolade <yemluck@gmail.com>
2025-03-22 17:19:31 -0500

    implemented optimized solution

--- List/sliding_window_probs/max_stock_profit.py
+++ List/sliding_window_probs/max_stock_profit.py
@@ -23,4 +23,19 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 
 
 def max_stock_profit(prices):
-    pass
+    l, r = 0, 1
+    maxP = 0
+
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            currentP = prices[r] - prices[l]
+            maxP = max(maxP, currentP)
+        else:
+            l = r
+        r += 1
+
+    return maxP
+
+print(max_stock_profit([7,1,5,3,6,4]))
+print(max_stock_profit([7,6,4,3,1]))
+
